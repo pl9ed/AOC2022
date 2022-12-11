dac3b838bf0948e36d449e96c4e04304b63463c5
pl9ed <pl9ed15@gmail.com>
2022-12-11 13:38:00 -0500

    Moved some methods over to monkey class

--- src/main/kotlin/completed/Day11.kt
+++ src/main/kotlin/completed/Day11.kt
@@ -19,51 +19,12 @@ class Day11(val input: File) {
     private val monkeys = mutableListOf<Monkey>()
 
     init {
+        resetState()
+    }
+
+    private fun resetState() {
         input.readText().split("\n\n").forEach { str ->
-            monkeys.add(str.toMonkey())
-        }
-    }
-
-    private fun String.toMonkey(): Monkey {
-        val lines = this.split("\n")
-
-        val items = mutableListOf<Int>()
-        lines[1].split(":")[1].trim().split(", ").forEach { item ->
-            items.add(item.toInt())
-        }
-        val inspect: (Int) -> Int = getOperation(lines[2].split(":")[1])
-        val divisibleBy: Int = lines[3].filter { it.isDigit() }.toInt()
-        val trueMonkey: Int = lines[4].filter { it.isDigit() }.toInt()
-        val falseMonkey: Int = lines[5].filter { it.isDigit() }.toInt()
-
-        val test: (Int) -> Int = { worry ->
-            if (worry % divisibleBy == 0) {
-                trueMonkey
-            } else {
-                falseMonkey
-            }
-        }
-
-        return Monkey(inspect, test, items)
-    }
-
-    private fun getOperation(string: String): (Int) -> Int {
-        val mutation = string.split("=")[1].trim().split(" ")
-        // should be old {operation} {param}
-        val operation: (Int, Int) -> Int = when (mutation[1]) {
-            "+" -> Int::plus
-            "-" -> Int::minus
-            "*" -> Int::times
-            "/" -> Int::div
-            else -> throw Exception("Unexpected operation ${mutation[1]}")
-        }
-        return { worry ->
-            try {
-                val value = mutation[2].toInt()
-                operation(worry, value) / 3
-            } catch (e: NumberFormatException) {
-                operation(worry, worry) / 3
-            }
+            monkeys.add(Monkey(str))
         }
     }
 
@@ -91,16 +52,63 @@ class Day11(val input: File) {
     }
 }
 
-class Monkey(
-    val inspect: (Int) -> Int,
-    val test: (Int) -> Int,
-    val items: MutableList<Int> = mutableListOf(),
-) {
+class Monkey {
+
+    val inspect: (Int) -> Int
+    val test: (Int) -> Int
+    var items = mutableListOf<Int>()
+
+    constructor(string: String) {
+        val lines = string.split("\n")
+
+        lines[1].split(":")[1].trim().split(", ").forEach { item ->
+            items.add(item.toInt())
+        }
+        inspect = getOperation(lines[2].split(":")[1])
+        val divisibleBy: Int = lines[3].filter { it.isDigit() }.toInt()
+        val trueMonkey: Int = lines[4].filter { it.isDigit() }.toInt()
+        val falseMonkey: Int = lines[5].filter { it.isDigit() }.toInt()
+
+        test = { worry ->
+            if (worry % divisibleBy == 0) {
+                trueMonkey
+            } else {
+                falseMonkey
+            }
+        }
+    }
+
+    constructor(inspect: (Int) -> Int, test: (Int) -> Int, items: MutableList<Int>) {
+        this.inspect = inspect
+        this.test = test
+        this.items = items
+    }
+
     fun parseItem(): Array<Int> {
         val item = items.removeFirst()
-        val worry = inspect(item)
+        val worry = inspect(item) / 3
         val target = test(worry)
 
         return arrayOf(target, worry)
+    }
+
+    private fun getOperation(string: String): (Int) -> Int {
+        val mutation = string.split("=")[1].trim().split(" ")
+        // should be old {operation} {param}
+        val operation: (Int, Int) -> Int = when (mutation[1]) {
+            "+" -> Int::plus
+            "-" -> Int::minus
+            "*" -> Int::times
+            "/" -> Int::div
+            else -> throw Exception("Unexpected operation ${mutation[1]}")
+        }
+        return { worry ->
+            try {
+                val value = mutation[2].toInt()
+                operation(worry, value)
+            } catch (e: NumberFormatException) {
+                operation(worry, worry)
+            }
+        }
     }
 }
