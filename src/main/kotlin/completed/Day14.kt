aaadda45bbd972979d8c4935adaba1a2b0b42c03
pl9ed <pl9ed15@gmail.com>
2022-12-17 09:41:06 -0500

    Added day14 p2

--- src/main/kotlin/completed/Day14.kt
+++ src/main/kotlin/completed/Day14.kt
@@ -2,13 +2,26 @@ package completed
 
 import java.io.File
 import java.lang.IndexOutOfBoundsException
+import kotlin.math.abs
 
 fun main() {
-    Day14(Inputs.getFile("example14.txt")).apply {
-        part1()
-        printGrid()
-    }
+
+    // TODO: clean this up so p1/p2 use same impl
+
+    // Day14(Inputs.getFile("example14.txt")).apply {
+    //     part1()
+    //     printGrid()
+    // }
+    // Day14(Inputs.getFile("example14.txt")).apply {
+    //     part2()
+    //     printGrid2()
+    // }
+
     Day14(Inputs.getFile("input14.txt")).part1()
+    Day14(Inputs.getFile("input14.txt")).apply {
+        part2()
+        printGrid2()
+    }
 }
 
 class Day14(input: File) {
@@ -23,6 +36,7 @@ class Day14(input: File) {
     private val height: Int
 
     private val grid: Array<Array<Char>>
+    private val grid2 = mutableMapOf<Pair<Int, Int>, Char>()
 
     var fallingPoint: List<Int>
     var sand: Int = 0
@@ -59,20 +73,20 @@ class Day14(input: File) {
             }
         }
 
-        println("Coordinates:")
-        walls.forEach {
-            println(it.joinToString())
-        }
+        // println("Coordinates:")
+        // walls.forEach {
+        //     println(it.joinToString())
+        // }
 
         populateGrid()
 
-        println(fallingPoint.joinToString())
         grid[fallingPoint[1]][fallingPoint[0]] = '+'
+        grid2[Pair(fallingPoint[1], fallingPoint[0])] = '+'
 
-        println("Grid:")
-        grid.forEach {
-            println(it.contentToString())
-        }
+        // println("Grid:")
+        // grid.forEach {
+        //     println(it.contentToString())
+        // }
     }
 
     fun part1() {
@@ -82,16 +96,42 @@ class Day14(input: File) {
                 sand++
             }
         } catch (e: IndexOutOfBoundsException) {
-            println(e)
             println("Sand: $sand")
         }
+        println(sand)
+    }
 
+    fun part2() {
+        while (grid2(fallingPoint[0], fallingPoint[1]) == '+') {
+            drop2(fallingPoint[0], fallingPoint[1])
+            sand++
+        }
+
+        println(sand)
     }
 
     fun printGrid() {
         println("Grid state:")
         grid.forEach {
             println(it.contentToString())
+        }
+    }
+
+    fun printGrid2() {
+        val x = grid2.keys.maxOf { it.first }
+        val y = grid2.keys.maxOf { it.second }
+        val offset = abs(grid2.keys.minOf { it.second })
+        val arr: Array<Array<Char>> = Array(x + 4) { Array(y + offset + 1) { '.' } }
+
+        grid2.forEach { (key, value) ->
+            arr[key.first][key.second + offset] = value
+        }
+
+        val file = File("src/main/resources/grid2.txt")
+        file.createNewFile()
+        arr.forEach {
+            println(it.contentToString())
+            file.appendText(it.contentToString() + "\n")
         }
     }
 
@@ -103,8 +143,22 @@ class Day14(input: File) {
         grid[y][x] = 'o'
     }
 
+    private fun drop2(x: Int, y: Int) {
+        if (y >= yMax + 2) {
+            return
+        }
+        if (grid2(x, y + 1) == null) drop2(x, y + 1) // down
+        if (grid2(x - 1, y + 1) == null) drop2(x - 1, y + 1) // left
+        if (grid2(x + 1, y + 1) == null) drop2(x + 1, y + 1) // right
+        sand++
+        grid2[Pair(y, x)] = 'o'
+    }
+
     private fun grid(x: Int, y: Int) =
         grid[y][x]
+
+    private fun grid2(x: Int, y: Int) =
+        grid2[Pair(y, x)]
 
     private fun populateGrid() {
         walls.forEach { wall ->
@@ -116,10 +170,12 @@ class Day14(input: File) {
                     if (xRange.size > 1) {
                         for (x in xRange[0]..xRange[1]) {
                             grid[yRange[0]][x] = '#'
+                            grid2[Pair(yRange[0], x)] = '#'
                         }
                     } else {
                         for (y in yRange[0]..yRange[1]) {
                             grid[y][xRange[0]] = '#'
+                            grid2[Pair(y, xRange[0])] = '#'
                         }
                     }
                 } catch (e: Exception) {
